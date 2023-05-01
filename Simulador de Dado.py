9cb7214d4b80b3972f1d3f083018b2da314c339c
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-01 15:19:57 -0300

    classe

---
+++ Simulador de Dado.py
@@ -0,0 +1,11 @@
+#Simulador de Dado
+import random #Bibloteca que gera valores aleatorios
+
+class SimuladorDedado:
+    def __init__(self) -> None: #Objeto da Classe simulador de dado
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        self.mensagem = 'Você gostaria de Girar o Dado ?'
+    
+def Iniciar(self):
+    input(self.mensagem)
