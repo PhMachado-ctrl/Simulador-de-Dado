2d4d23e18c8f482639eaa5badd00be1460ede149
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-01 16:18:59 -0300

    Instanciar a classe

--- SimuladordeDado.py
+++ SimuladordeDado.py
@@ -5,7 +5,7 @@ class SimuladorDedado:
     def __init__(self) -> None: #Objeto da Classe simulador de dado
         self.valor_minimo = 1
         self.valor_maximo = 6
-        self.mensagem = 'Você gostaria de Girar o Dado ?'
+        self.mensagem = 'Você gostaria de Girar o Dado ? \n'
     
     def Iniciar(self):
         resposta = input(self.mensagem)
@@ -13,4 +13,8 @@ class SimuladorDedado:
             self.GerarValorDoDado()
 
     def GerarValorDoDado(self):
-        print(random.randint(self.valor_minimo, self.valor_maximo))
+        print(random.randint(self.valor_minimo, self.valor_maximo)) #Escolhe um valor aleatório entre ovalor minimo e maximo
+
+'''Para instanciar a classe'''
+simulador = SimuladorDedado()
+simulador.Iniciar()
