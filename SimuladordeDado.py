8e39d153d7c3864b4dc6f9f3ac311f27e8e68dec
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-01 17:32:55 -0300

    import PysimpleGUI

--- SimuladordeDado.py
+++ SimuladordeDado.py
@@ -1,19 +1,29 @@
 #Simulador de Dado
 import random #Bibloteca que gera valores aleatorios
+import PySimplGUI #Bliblioyeca de tela e interface
 
 class SimuladorDedado:
     def __init__(self) -> None: #Objeto da Classe simulador de dado
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = 'Você gostaria de Girar o Dado ? \n'
-    
+        # Layout
+       
+        # criar uma janela
+        # ler os valores da tela
+        # fazer alguma coisa com esses valores
+
     def Iniciar(self):
         resposta = input(self.mensagem)
-        if resposta == 'sim' or resposta == 's':
-            self.GerarValorDoDado()
-        elif resposta == 'não' or resposta == 'n':
-        else:
-            print('Favor Digitar sim ou não')
+        try:
+            if resposta == 'sim' or resposta == 's':
+                self.GerarValorDoDado()
+            elif resposta == 'não' or resposta == 'n':
+                print("Agradecemos a sua participação")
+            else:
+                print('Favor Digitar sim ou não')
+        except:
+            print("Ocorreu um erro ao receber uma resposta")
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo)) #Escolhe um valor aleatório entre ovalor minimo e maximo
