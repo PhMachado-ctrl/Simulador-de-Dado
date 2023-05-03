dd36b56945cd8fde97060648e41aa744489b91cf
PhMachado-ctrl <phmachado555@gmail.com>
2023-05-03 16:18:54 -0300

    Tela-e-Execico

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Simulador-de-Dado
+
+# Necessario instalar PySimpleGUI
+python -m pip install PySimpleGUI 
+# Atualizar o pip caso de problema
+python -m pip install --update pip
+# Executar
+python3 hello_world.py
--- SimuladordeDado.py
+++ SimuladordeDado.py
@@ -3,31 +3,33 @@ import random #Bibloteca que gera valores aleatorios
 import PySimpleGUI as sg #Bliblioyeca de tela e interface
 
 class SimuladorDedado:
-    def __init__(self) -> None: #Objeto da Classe simulador de dado
+    def __init__(self)-> None: #Objeto da Classe simulador de dado
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem = 'Você gostaria de Girar o Dado ? \n'
         # Layout
-        layout = [
-            [sg.Text("Jogar um Dado?")]
+        self.layout = [
+            [sg.Text("Jogar um Dado?")],
             [sg.Button("sim"), sg.Button("não")]
         ]
-        # criar uma janela
-        janela = sg.Window('Simulador de Dado', layout=layout)
-        # ler os valores da tela
+       
+      
         # fazer alguma coisa com esses valores
-
     def Iniciar(self):
-        resposta = input(self.mensagem)
-        try:
-            if resposta == 'sim' or resposta == 's':
-                self.GerarValorDoDado()
-            elif resposta == 'não' or resposta == 'n':
-                print("Agradecemos a sua participação")
-            else:
-                print('Favor Digitar sim ou não')
-        except:
-            print("Ocorreu um erro ao receber uma resposta")
+         # criar uma janela
+        self.janela = sg.Window('Simulador de Dado', layout=self.layout)
+          # ler os valores da tela
+        self.eventos, self.valores = self.janela.Read()
+        while True:
+            try:
+                if self.eventos == "sim" or self.eventos == 's':
+                    self.GerarValorDoDado()
+                elif self.eventos == "não" or self.eventos == 'n':
+                    print("Agradecemos a sua participação")
+                else:
+                    print('Favor Digitar sim ou não')
+            except:
+                print("Ocorreu um erro ao receber uma resposta")
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo, self.valor_maximo)) #Escolhe um valor aleatório entre ovalor minimo e maximo
